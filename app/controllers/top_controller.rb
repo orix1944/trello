0e0765fb4657bbc5b92f864a0df01217a907ae15
orix1944 <bf109.sai@gmail.com>
2019-05-13 22:20:50 +0900

    createlist

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,2 +1,8 @@
 class TopController < ApplicationController
+
+  def index
+    @lists = List.where(user_id: current_user).order("created_at DESC")
+  end
+
+
 end
---
+++ app/models/list.rb
@@ -0,0 +1,10 @@
+class List < ApplicationRecord
+
+  include PublicActivity::Model
+  tracked owner: proc { |controller, _model| controller.current_user }
+  tracked recipient: proc { |controller, _model| controller.current_user }
+
+  belongs_to :user
+  has_many :cards, dependent: :destroy
+  validates :title, length: {in: 1..255}
+end
