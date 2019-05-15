82cc9e29c8790e96b050b0a85e742a75be5f994b
orix1944 <bf109.sai@gmail.com>
2019-05-15 15:32:20 +0900

    name:integertostring

--- db/schema.rb
+++ db/schema.rb
@@ -90,7 +90,7 @@ ActiveRecord::Schema.define(version: 2019_05_14_150651) do
     t.string "uid"
     t.integer "level", default: 0
     t.string "title", default: "5級"
-    t.integer "name"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
